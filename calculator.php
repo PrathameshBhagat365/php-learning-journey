8c642dcb050498241b7243dfccb6e3c7c1a15cf6
PrathameshBhagat365 <prathameshbhagat1511@gmail.com>
2025-07-13 11:46:55 +0530

    calculator.php

---
+++ calculator.php
@@ -0,0 +1,9 @@
+<?php
+$num1 = 15;
+$num2 = 5;
+
+echo "Addition: " . ($num1 + $num2) . "<br>";
+echo "Subtraction: " . ($num1 - $num2) . "<br>";
+echo "Multiplication: " . ($num1 * $num2) . "<br>";
+echo "Division: " . ($num1 / $num2) . "<br>";
+?>
