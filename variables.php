7f2c933633a5a25f9f19854aaa0b42f08b896b61
PrathameshBhagat365 <prathameshbhagat1511@gmail.com>
2025-07-13 11:45:37 +0530

    Create variables.php

---
+++ variables.php
@@ -0,0 +1,17 @@
+<?php
+$name = "Prathamesh";
+$age = 21;
+$price = 49.99;
+$isStudent = true;
+
+echo "Hello, my name is " . $name . "<br>";
+echo "I am " . $age . " years old.<br>";
+
+$sum = 10 + 20;
+echo "10 + 20 = " . $sum . "<br>";
+
+define("SITE_NAME", "Mindseed Tutorials");
+echo "Welcome to " . SITE_NAME . "<br>";
+
+var_dump($isStudent);
+?>
