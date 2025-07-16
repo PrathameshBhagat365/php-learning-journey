834c605e4d615ece007e209b89a8734ff9f10fe6
PrathameshBhagat365 <prathameshbhagat1511@gmail.com>
2025-07-16 12:36:26 +0530

    form_get.php

---
+++ form_get.php
@@ -0,0 +1,15 @@
+<!-- form_get.php -->
+<!DOCTYPE html>
+<html>
+<head>
+    <title>GET Form Example</title>
+</head>
+<body>
+    <h2>Feedback Form (GET)</h2>
+    <form action="form_result.php" method="get">
+        Name: <input type="text" name="username"><br><br>
+        Message: <textarea name="message"></textarea><br><br>
+        <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
