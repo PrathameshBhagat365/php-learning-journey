82cb563d610a3f2b325b4da1c1b87002e8c3a1cc
PrathameshBhagat365 <prathameshbhagat1511@gmail.com>
2025-07-19 17:45:59 +0530

    form_clean.php

---
+++ form_clean.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head><title>Contact Form</title></head>
+<body>
+    <h2>Contact Us</h2>
+    <form method="post" action="form_clean_result.php">
+        Name: <input type="text" name="name"><br><br>
+        Email: <input type="text" name="email"><br><br>
+        Message:<br>
+        <textarea name="message"></textarea><br><br>
+        <input type="submit" value="Send">
+    </form>
+</body>
+</html>
